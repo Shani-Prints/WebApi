9bb442b669c7110eba48dab34f69a765c6369dc1
ShaniPrints <s0527621806@gmail.com>
2024-12-18 23:43:42 +0200

    Initial commit

---
+++ Models/Jewelry.cs
@@ -0,0 +1,8 @@
+namespace WebApi.Models;
+public class Jewelry
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double Price { get; set; }
+    public string? Category { get; set; }
+}
