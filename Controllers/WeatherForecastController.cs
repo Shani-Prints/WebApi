ff33c3e53fec32594b428cfd37279ccb27fd98c5
ShaniPrints <s0527621806@gmail.com>
2024-12-25 00:59:27 +0200

    .net8

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
-
 namespace WebApi.controllers;
-
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
